760d5993417d3bb032e827ccfb5b11e24b39dcb0
MorganRohan <10355@coderacademy.edu.au>
2022-04-19 17:41:37 +1000

    Tuesdaycommit: initial styling for headings + menu bugs

--- src/bookapp.rb
+++ src/bookapp.rb
@@ -2,25 +2,29 @@
 #Gems required:
 require "tty-prompt"
 require "colorize"
-
+require "tty-font"
+require "artii"
+require "pastel"
 
 #Local files required:
 require_relative ("./review.rb")
 require_relative ("./recommendationquiz.rb")
 require_relative ("./readinglist.rb")
+require_relative ("./styling.rb")
 
 #Define a method to get input for new review
 #Print format of review defined in review class
 def new_review 
-    puts "Your book review details"
+    puts "Let's start a new book review!".blue
+    puts "Your book review details".blue
     sleep 1
-    puts "Enter your book title: "
+    puts "Enter your book title: ".blue
     title = gets.chomp.to_s
     sleep 1
-    puts "Enter your rating (out of 5): "
+    puts "Enter your rating (out of 5): ".blue
     rating = gets.chomp.to_s
     sleep 1
-    puts "Enter your review: "
+    puts "Enter your review: ".blue 
     review = gets.chomp.to_s
     sleep 1
     new_review = Reviews.new(title, rating, review)
@@ -43,17 +47,22 @@ def display_all_reviews
     puts all_reviews
 end
 
+def delete_all_reviews
+    all_reviews = File.write("reviews.txt", " ")
+    puts "You deleted all your reviews :(".red
+end
+
 #Define a method to open the reading list
 #Run new list item method from readinglist file 
 #Take user inputs and print result
 def add_list
-    puts "Your Current Reading List!"
-    puts "Let's add some titles..."
+    puts "This is your reading list, where you can add all the titles you want to read!".blue
+    puts "Let's add some titles...".blue
     sleep 1
-    puts "What is the book title?"
+    puts "What is the book title?".blue
     title = gets.chomp.to_s
     sleep 1
-    puts "What is the book author?"
+    puts "What is the book author?".blue
     author = gets.chomp.to_s
     list_entry = List.new(title, author) 
     puts list_entry.to_s
@@ -73,21 +82,31 @@ def display_reading_list
     puts reading_list
 end
 
+def delete_all_list
+    reading_list = File.write("readinglist.txt", " ")
+    puts "You cleared your reading list :(".red
+end
+
 #Define a method to start recommendation quiz
 #Welcome to the quiz statement and input specification
 def recommendation_quiz
-    puts "Welcome to the book recommendation quiz!"
-    puts "Please answer the following questions by typing 'a' or 'b'"
+    puts "Welcome to the book recommendation quiz!".red
+    puts "Please answer the following questions by typing 'a' or 'b'".red
     new_quiz = Quiz.new($questions)
 end
 
 def review_menu
-    answer = $prompt.select("Would you like to add another review?",["Yes", "No", "View All Reviews"])
+    answer = $prompt.select("Would you like to add another review?",["Yes", "No", "View All Reviews", "Delete All Reviews"])
     answer
 end
 
 def list_menu
-    answer = $prompt.select("Would you like to add more list items?",["Yes", "No", "View List"])
+    answer = $prompt.select("Would you like to add more list items?",["Yes", "No", "View List", "Delete All List Entries"])
+    answer
+end
+
+def recommendation_menu
+    answer = $prompt.select("Now that you've seen some recommendations, would you like to add to your Reading List?", ["Yes", "No"])
     answer
 end
 
@@ -98,46 +117,64 @@ def main_menu
 end
 
 system "clear"
-puts "Welcome to the Book App"
+main_welcome
 option = ""
 while option !="Exit App"
     option = main_menu
     case option
     when "New Book Review"
+        reviews_heading
         new_review
         while option!= "No"
             option = review_menu
             case option
                 when "Yes"
+                    reviews_heading
                     new_review
                 when "View All Reviews"
+                    display_reviews_heading
                     display_all_reviews
+                when "Delete All Reviews"
+                    delete_all_reviews
                 else
-                    puts "Back to main menu it is!"
+                    puts "Back to main menu it is!".magenta
                 end
             end 
     when "Reading List"
+        list_heading
         add_list
         while option!= "No"
             option = list_menu
             case option
                 when "Yes"
+                    list_heading
                     add_list
                 when "View List"
+                    display_list_heading
                     display_reading_list
+                when "Delete All List Entries"
+                    delete_all_list
                 else
                     puts "Back to main menu it is!"
                 end
             end
     when "Take Recommendation Quiz"
+        quiz_heading
+        sleep 1
         quiz = recommendation_quiz
         puts quiz.run_quiz($questions)
-        puts "Now that you've gotten recommendations, would you like to add a new title to your Reading List?"
+        recommendation_menu
         while option!= "No"
             option = list_menu
             case option
                 when "Yes"
+                    list_heading
                     add_list
+                when "View List"
+                    display_list_heading
+                    display_reading_list
+                when "Delete All List Entries"
+                    delete_all_list
                 else
                     puts "Back to main menu it is!"
                 end
---
+++ src/styling.rb
@@ -0,0 +1,51 @@
+#Separate page for styling elements that can be used in the app
+#Kept in a separate file so that some of the headings or other repeatable elements can be established as methods
+#This allows for calling a method rather then adding the style to the heading each time (repeatable)
+
+#Gems required
+require "tty-font"
+require "pastel"
+require "artii"
+require "colorize"
+
+def main_welcome
+    font = TTY::Font.new(:doom)
+    pastel = Pastel.new
+    puts pastel.magenta(font.write("Welcome to the BookShelf App!"))
+end
+
+def reviews_heading
+    arter = Artii::Base.new
+    puts arter.asciify("Book Review!").blue
+end 
+
+def list_heading
+    arter = Artii::Base.new
+    puts arter.asciify("Reading List!").green
+end 
+
+def quiz_heading
+    arter = Artii::Base.new
+    puts arter.asciify("Welcome....").red
+    sleep 1
+    puts arter.asciify("To....").red
+    sleep 1
+    puts arter.asciify("The...").red
+    sleep 1
+    puts arter.asciify("...Recommendation Quiz!").red
+end
+
+def exit_message
+    arter = Artii::Base.new
+    puts arter.asciify("Thank you!").yellow
+end
+
+def display_reviews_heading
+    arter = Artii::Base.new
+    puts arter.asciify("Check Out Your Reviews!").blue
+end
+
+def display_list_heading
+    arter = Artii::Base.new
+    puts arter.asciify("Check Out Your Reading List!").green
+end
